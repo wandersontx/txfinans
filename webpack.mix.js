a152a616c55a560aff5548aff456d2fa7a1a93bc
wandersontx <wandersonh89@gmail.com>
2021-01-26 00:29:16 -0300

    Autenticaçao autenticaçao padrao laravel instalaçao do Jquery e bootstrap compilaçao do sass

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/views/scss/style.scss','public/site/style.css')
+    .scripts('node_modules/jquery/dist/jquery.js','public/site/jquery.js');
